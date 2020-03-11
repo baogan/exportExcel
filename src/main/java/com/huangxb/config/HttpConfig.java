98c0ffeec5abc9c839867358ea108b3d11d076d7
baogan <huangxiaobin54@gmail.com>
2020-03-11 11:30:44 +0800

    his is http config values

--- src/main/java/com/huangxb/config/HttpConfig.java
+++ src/main/java/com/huangxb/config/HttpConfig.java
@@ -4,7 +4,7 @@ package com.huangxb.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
+// this is http config values
 @Component
 @ConfigurationProperties(prefix = "http")
 public class HttpConfig {
